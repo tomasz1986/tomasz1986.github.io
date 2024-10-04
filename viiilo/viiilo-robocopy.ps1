2b90a8bf0758e0505619abb0d825b42b22b6ceaa
Tomasz Wilczyński <twilczynski@naver.com>
2024-10-04 08:05:39 +0200

    Initial commit

--- viiilo/viiilo-robocopy.ps1
+++ viiilo/viiilo-robocopy.ps1
@@ -1,7 +1,13 @@
+$Robocopy = if ($env:robocopy) {
+	$env:robocopy
+} else {
+	"robocopy.exe"
+}
+
 $Source = $PSScriptRoot
 $Destination = Join-Path $env:USERPROFILE -ChildPath "Desktop\Pulpit\Biuro\WWW\twilczynski.com\viiilo"
 if (Test-Path -LiteralPath $Destination) {
-	$Command = $env:robocopy + " /mir `"" + $Source + "`" `"" + $Destination + "`""
+	$Command = $Robocopy + " /mir `"" + $Source + "`" `"" + $Destination + "`""
 	Invoke-Expression -Command $Command
 }
 
